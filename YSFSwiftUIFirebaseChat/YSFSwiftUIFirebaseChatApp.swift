09bda4392620e082404662086282c5c4475c6733
YUSUFPELTEK <yusufpeltek0@gmail.com>
2023-04-18 01:22:29 +0300

    Initial Commit

---
+++ YSFSwiftUIFirebaseChat/YSFSwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  YSFSwiftUIFirebaseChatApp.swift
+//  YSFSwiftUIFirebaseChat
+//
+//  Created by YUSUF on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct YSFSwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
